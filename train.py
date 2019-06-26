9ce23d85305000722d01ca17efa9dde6cccbd6fc
Peilin Rao <pelinium@gmail.com>
2019-06-26 14:12:53 -0500

    OK

---
+++ train.py
@@ -0,0 +1,10 @@
+import torch
+import utils
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+import torch.optim as optim
+import numpy as np
+from torch.autograd import Variable
+import generator
+import discriminator
