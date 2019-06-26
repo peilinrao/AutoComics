02f29d94c721593342e50eb6f61b445d35a9435a
ZQZCalin <zqz20100221@126.com>
2019-06-26 15:39:45 -0400

    generator finished, discriminator typo fixed

--- discriminator.py
+++ discriminator.py
@@ -9,22 +9,22 @@ class discriminator(nn.Module):
         self.output_channel = out_size
         self.layer_input_size = n
         self.conv = nn.Sequential(            
-            nn.Conv2d(in_chn, n * 1, kernal_size=3, stride=1, padding=1),
+            nn.Conv2d(in_chn, n * 1, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(n * 1, n * 2, kernal_size=3, stride=2, padding=1),
+            nn.Conv2d(n * 1, n * 2, kernel_size=3, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(n * 2, n * 4, kernal_size=3, stride=1, padding=1),
+            nn.Conv2d(n * 2, n * 4, kernel_size=3, stride=1, padding=1),
             nn.InstanceNorm2d(n * 4),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(n * 4, n * 4, kernal_size=3, stride=2, padding=1),
+            nn.Conv2d(n * 4, n * 4, kernel_size=3, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(n * 4, n * 8, kernal_size=3, stride=1, padding=1),
+            nn.Conv2d(n * 4, n * 8, kernel_size=3, stride=1, padding=1),
             nn.InstanceNorm2d(n * 4),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(n * 8, n * 8, kernal_size=3, stride=1, padding=1),
+            nn.Conv2d(n * 8, n * 8, kernel_size=3, stride=1, padding=1),
             nn.InstanceNorm2d(n * 4),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(n * 8, out_chn, kernal_size=3, stride=1, padding=1),
+            nn.Conv2d(n * 8, out_chn, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid()
         )
 
--- generator.py
+++ generator.py
@@ -1,27 +1,82 @@
 import torch
-import utils
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.autograd as autograd
-import torch.optim as optim
-import numpy as np
-from torch.autograd import Variable
 
-class generator_nn(nn.Module):
-    def __init__():
-        super(generator_nn, self).__init__()
-        num_feature = 64
-        down_convolution_seq = nn.Sequential(
-          nn.Conv2d(in_channel, num_feature, kernel_size=7, stride=1,padding = 1),
-          nn.InstanceNorm2d(num_feature),
-          nn.ReLU(),
-          nn.Conv2d(num_feature, num_feature*2, kernel_size=3, stride=2,padding = 1),
-          nn.Conv2d(num_feature*2, num_feature*2, kernel_size=3, stride=1,padding = 1),
-          nn.ReLU()
-          nn.Conv2d(num_feature*2, num_feature*4, kernel_size=3, stride=2,padding = 1),
-          nn.Conv2d(num_feature*4, num_feature*4, kernel_size=3, stride=1,padding = 1),
-          nn.InstanceNorm2d(num_feature*4),
-          nn.ReLU()
+class resnet_block(nn.Module):
+    def __init__(self, nf, kernel_size, stride, padding):
+        super(resnet_block, self).__init__()
+        self.channel = channel
+        self.kernel = kernel
+        self.stride = stride
+        self.padding = padding
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(nf, nf, kernel_size, stride, padding)
+            nn.InstanceNorm2d(nf)
+            nn.ReLU(True)
+            nn.Conv2d(nf, nf, kernel_size, stride, padding)
+            nn.InstanceNorm2d(nf)
         )
 
-        self.resnet
+        utils.initialize_weights(self)
+
+    def forward(self, input):
+        output = input + self.conv(input)
+
+        return output
+
+class generator_nn(nn.Module):
+    def __init__(self, in_chn, out_chn, nf=64, nb=8):
+        # parameters
+        super(generator_nn, self).__init__()
+        self.input_channel = in_chn
+        self.output_channel = out_chn
+        self.feature_num = nf
+        self.resnet_block_num = batch_size
+
+        # down-convolution 
+        self.down_conv = nn.Sequential(
+          nn.Conv2d(in_chn, nf, kernel_size=7, stride=1, padding = 1),
+          nn.InstanceNorm2d(nf),
+          nn.ReLU(True),
+          nn.Conv2d(nf * 1, nf * 2, kernel_size=3, stride=2, padding = 1),
+          nn.Conv2d(nf * 2, nf * 2, kernel_size=3, stride=1, padding = 1),
+          nn.ReLU(True)
+          nn.Conv2d(nf * 2, nf * 4, kernel_size=3, stride=2, padding = 1),
+          nn.Conv2d(nf * 4, nf * 4, kernel_size=3, stride=1, padding = 1),
+          nn.InstanceNorm2d(nf * 4),
+          nn.ReLU(True)
+        )
+
+        # 8 residual blocks
+        self.resnet_blocks = []
+        for i in range(nb):
+            self.resnet_blocks.append(resnet_block(nf * 4, kernel_size=3, stride=1, padding=1))
+
+        self.resnet = nn.Sequential(*self.resnet_blocks)
+
+        # check this alternative code works
+        # self.resnet = nn.Sequential([resnet_block(nf * 4, kernel_size=3, stride=1, padding=1) for i range(nb)])
+
+        # up-convolution
+        self.up_conv = nn.Sequential(
+            nn.ConvTranspose2d(nf * 4, nf * 2, 3, 2, 1, 1)
+            nn.Conv2d(nf * 2, nf * 2, kernel_size=3, stride=1, padding=1)
+            nn.InstanceNorm2d(nf * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(nf * 2, nf, 3, 2, 1, 1)
+            nn.Conv2d(nf, nf, kernel_size=3, stride=1, padding=1)
+            nn.InstanceNorm2d(nf),
+            nn.ReLU(True),
+            nn.Conv2d(nf, out_chn, kernel_size=7, stride=1, padding=3)
+            nn.Tanh()
+        )
+
+        util.initialize_weights(self)
+
+    def forward(self, input):
+        temp = self.down_conv(input)
+        temp = self.resnet(temp)
+        output = self.up_conv(temp)
+
+        return output
